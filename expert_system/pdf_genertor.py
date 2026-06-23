b7800f16badf39f442220924cb9c93f3d19c0099
nguyen-vlu <nguyen.2474802010261@vanlanguni.vn>
2026-06-23 22:41:51 +0700

    update

---
+++ expert_system/pdf_genertor.py
@@ -0,0 +1,24 @@
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
+import textwrap
+
+def remove_accents(input_str):
+    """ Hàm hỗ trợ bỏ dấu tiếng Việt để tránh lỗi font ReportLab mặc định khi không có font Unicode """
+    s1 = u'ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
+    s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
+    s = ''
+    for c in input_str:
+        if c in s1:
+            s += s0[s1.index(c)]
+        else:
+            s += c
+    return s
+
+def generate_pdf(data, filepath):
+    c = canvas.Canvas(filepath, pagesize=A4)
+    width, height = A4
+    y = height - 50
+
+    c.setFont("Helvetica-Bold", 16)
+    c.drawString(50, y, "Bao cao Chan doan Benh Ho hap (Tham khao)")
+    y -= 30
